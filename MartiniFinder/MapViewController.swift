1acb7aa78e251bf462cdef0c397b72644e10092f
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-05 21:43:31 -0800

    Create MapViewController class and assign in storyboard

---
+++ MartiniFinder/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 1/5/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class MapViewController: UIViewController {
+    
+}
