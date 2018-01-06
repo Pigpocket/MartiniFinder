528c8a67dd20fc99eded903c2dacce1e20c7e3af
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-05 21:36:12 -0800

    Implement tab bar controller views

---
+++ MartiniFinder/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 1/4/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var myMartiniBarsOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+//    @IBAction func findMartinisAction(_ sender: Any) {
+//        completeLogin()
+//    }
+//
+//
+//    func completeLogin() {
+//        performUIUpdatesOnMain {
+//            let controller = self.storyboard!.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+//            self.present(controller, animated: true, completion: nil)
+//        }
+//    }
+}
+
