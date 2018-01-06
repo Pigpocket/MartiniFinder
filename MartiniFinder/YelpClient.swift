9ae8edad6e1261935e14e677a0814b12948f333d
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-05 22:24:38 -0800

    Add client class

---
+++ MartiniFinder/YelpClient.swift
@@ -0,0 +1,15 @@
+//
+//  YelpClient.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 1/5/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+
+class YelpClient {
+    
+    let request = String("GET https://api.yelp.com/v3/businesses/search")
+    
+}
