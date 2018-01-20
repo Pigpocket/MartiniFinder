6cabd311ccf04c84a51184527fec35be56bf78e2
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-01-20 15:35:07 -0800

    Implement setMapRegion function

--- MartiniFinder/MapViewController.swift
+++ MartiniFinder/MapViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 import MapKit
 import Foundation
+import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
     
     // MARK: Properties
     
     let annotation = MKPointAnnotation()
     let annotationArray: [MKPointAnnotation] = []
+    var locationManager = CLLocationManager()
     
     // MARK: Outlets
     
@@ -24,6 +26,53 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         
         self.mapView.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        setMapRegion()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
+    func setAnnotations() {
+        
+        // Set the coordinates
+        let coordinates = CLLocationCoordinate2D(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+        
+        // Set the map region
+        let region = MKCoordinateRegionMake(coordinates, MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        self.mapView.delegate = self
+        
+        // Set the annotation
+        annotation.coordinate = coordinates
+        
+        // Add the annotation
+        mapView.addAnnotation(self.annotation)
+        self.mapView.addAnnotation(self.annotation)
+    }
+    
+    func setMapRegion() {
+        
+        // Set the coordinates
+        let coordinates = CLLocationCoordinate2D(latitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!)
+        print("Coordinates are: \(coordinates)")
+        
+        let region = MKCoordinateRegionMake(coordinates, MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
     }
     
     
