f3ed0b32e725a59fbd16dad3cd5107346e5d5291
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-02-12 17:52:06 -0800

    Add and configure FavoritesViewController and FavoritesTableViewCell

---
+++ MartiniFinder/FavoritesTableViewCell.swift
@@ -0,0 +1,88 @@
+//
+//  FavoritesTableViewCell.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 2/12/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var horizontalStackView: UIStackView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var star1: UIImageView!
+    @IBOutlet weak var star2: UIImageView!
+    @IBOutlet weak var star3: UIImageView!
+    @IBOutlet weak var star4: UIImageView!
+    @IBOutlet weak var star5: UIImageView!
+    @IBOutlet weak var blankView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var openLabel: UILabel!
+    
+    
+    func displayRating(location: Location) {
+        
+        if location.rating == 1 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "emptyStar")
+            star3.image = UIImage(named: "emptyStar")
+            star4.image = UIImage(named: "emptyStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 1.5 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "halfStar")
+            star3.image = UIImage(named: "emptyStar")
+            star4.image = UIImage(named: "emptyStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 2 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "emptyStar")
+            star4.image = UIImage(named: "emptyStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 2.5 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "halfStar")
+            star4.image = UIImage(named: "emptyStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 3.0 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "filledStar")
+            star4.image = UIImage(named: "emptyStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 3.5 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "filledStar")
+            star4.image = UIImage(named: "halfStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 4.0 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "filledStar")
+            star4.image = UIImage(named: "filledStar")
+            star5.image = UIImage(named: "emptyStar")
+        } else if location.rating == 4.5 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "filledStar")
+            star4.image = UIImage(named: "filledStar")
+            star5.image = UIImage(named: "halfStar")
+        } else if location.rating == 5.0 {
+            star1.image = UIImage(named: "filledStar")
+            star2.image = UIImage(named: "filledStar")
+            star3.image = UIImage(named: "filledStar")
+            star4.image = UIImage(named: "filledStar")
+            star5.image = UIImage(named: "filledStar")
+        }
+    }
+    
+}
---
+++ MartiniFinder/FavoritesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FavoritesViewController.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 2/12/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class FavoritesViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    // Properties
+    
+    var favorites: [Favorite]?
+    
+    
+    
+    // Outlets
+    
+    @IBOutlet var favoritesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites!.count
+    }
+    
+    
+    
+}
