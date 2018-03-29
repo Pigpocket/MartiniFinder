75400a146be96b799fe71ecf52468b893358d862
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-03-28 22:20:32 -0700

    Refactor: move double rounding function to separate file

---
+++ MartiniFinder/View Controllers/DoubleRounding.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleRounding.swift
+//  MartiniFinder
+//
+//  Created by Tomas Sidenfaden on 3/28/18.
+//  Copyright © 2018 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
--- MartiniFinder/View Controllers/TableViewController.swift
+++ MartiniFinder/View Controllers/TableViewController.swift
@@ -174,19 +174,3 @@ extension TableViewController {
     }
 }
 
-//extension String {
-//
-//    func widthOfString(usingFont font: UIFont) -> CGFloat {
-//        let fontAttributes = [NSAttributedStringKey.font: font]
-//        let size = self.size(withAttributes: fontAttributes)
-//        return size.width
-//    }
-//}
-
-extension Double {
-    /// Rounds the double to decimal places value
-    func rounded(toPlaces places:Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
-    }
-}
